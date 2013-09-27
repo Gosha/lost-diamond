92e01ded9bf35d6f1bbeddbd887fbac2189e83b4
Georgij Zacharov <gosha.za@gmail.com>
2013-09-27 21:14:58 +0200

    Seems JPanel doesn't need the Scrollable interface

--- src/se/liu/ida/geoza435/tddc69/project/GUI/BoardDisplay.java
+++ src/se/liu/ida/geoza435/tddc69/project/GUI/BoardDisplay.java
@@ -8,7 +8,7 @@ import javax.swing.JPanel;
 
 import se.liu.ida.geoza435.tddc69.project.game.Board;
 
-public class BoardDisplay extends JPanel implements Scrollable {
+public class BoardDisplay extends JPanel  {
 	Board board;
 
 	public BoardDisplay(Board board) {
@@ -17,37 +17,4 @@ public class BoardDisplay extends JPanel implements Scrollable {
 		this.setSize(400, 400);
 		this.setPreferredSize(new Dimension(400, 400));
 	}
-
-	@Override
-	public Dimension getPreferredScrollableViewportSize() {
-		// TODO Auto-generated method stub
-		return new Dimension(300, 300);
-	}
-
-	@Override
-	public int getScrollableBlockIncrement(Rectangle visibleRect,
-			int orientation, int direction) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean getScrollableTracksViewportHeight() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean getScrollableTracksViewportWidth() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public int getScrollableUnitIncrement(Rectangle visibleRect,
-			int orientation, int direction) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 }
