f64ef70b877af0e909710ce21438a631d9a147f4
Georgij Zacharov <gosha.za@gmail.com>
2013-10-14 16:24:39 +0200

    Start of glass pane layout

--- src/se/liu/ida/geoza435/tddc69/project/GUI/game/GameView.java
+++ src/se/liu/ida/geoza435/tddc69/project/GUI/game/GameView.java
@@ -1,8 +1,12 @@
 package se.liu.ida.geoza435.tddc69.project.GUI.game;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import se.liu.ida.geoza435.tddc69.project.game.Game;
@@ -26,6 +30,25 @@ public class GameView extends JFrame {
 		spane.getVerticalScrollBar().setUnitIncrement(16);
 
 		add(spane, BorderLayout.CENTER);
+
+		JPanel p = new JPanel();
+		JButton b = new JButton("Hej");
+		JButton bb = new JButton("Hej");
+		JButton bbb = new JButton("Hej");
+
+		b.setBounds(0, 0, 40, 40);
+		p.setBounds(0, 0, 100, 100);
+		p.setSize(100, 100);
+		p.setOpaque(false);
+		p.setLayout(new GridBagLayout());
+		p.add(b);
+		p.add(bb);
+		p.add(bbb);
+		p.setBackground(new Color(100, 100, 100, 10));
+		getRootPane().getGlassPane().setVisible(true);
+
+		getRootPane().setGlassPane(p);
+
 		// TODO: Constants
 		setSize(600, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
