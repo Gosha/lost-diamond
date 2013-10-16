4a15f6a34a23943e8848bb90bb9e4b826d0de196
Georgij Zacharov <gosha.za@gmail.com>
2013-10-16 20:13:48 +0200

    simple StatusDisplay

--- src/se/liu/ida/geoza435/tddc69/project/GUI/game/GameView.java
+++ src/se/liu/ida/geoza435/tddc69/project/GUI/game/GameView.java
@@ -1,12 +1,8 @@
 package se.liu.ida.geoza435.tddc69.project.GUI.game;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.GridBagLayout;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.WindowConstants;
 
@@ -32,23 +28,9 @@ public class GameView extends JFrame {
 
 		add(spane, BorderLayout.CENTER);
 
-		JPanel p = new JPanel();
-		JButton b = new JButton("Hej");
-		JButton bb = new JButton("Hej");
-		JButton bbb = new JButton("Hej");
-
-		b.setBounds(0, 0, 40, 40);
-		p.setBounds(0, 0, 100, 100);
-		p.setSize(100, 100);
-		p.setOpaque(false);
-		p.setLayout(new GridBagLayout());
-		p.add(b);
-		p.add(bb);
-		p.add(bbb);
-		p.setBackground(new Color(100, 100, 100, 10));
-		getRootPane().getGlassPane().setVisible(true);
-
-		getRootPane().setGlassPane(p);
+		StatusDisplay status = new StatusDisplay(game);
+		setGlassPane(status);
+		status.setVisible(true);
 
 		// TODO: Constants
 		setSize(600, 500);
---
+++ src/se/liu/ida/geoza435/tddc69/project/GUI/game/StatusDisplay.java
@@ -0,0 +1,79 @@
+package se.liu.ida.geoza435.tddc69.project.GUI.game;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import se.liu.ida.geoza435.tddc69.project.Observable;
+import se.liu.ida.geoza435.tddc69.project.Observer;
+import se.liu.ida.geoza435.tddc69.project.game.Game;
+import se.liu.ida.geoza435.tddc69.project.game.Player;
+
+@SuppressWarnings("serial")
+public class StatusDisplay extends JPanel implements Observer {
+
+	JLabel label;
+	JPanel panel;
+	private Game game;
+
+	public StatusDisplay(Game game) {
+		this.game = game;
+		setLayout(null);
+
+		panel = new JPanel();
+		panel.setLayout(new GridLayout(0, 1));
+		panel.setBounds(20, 20, 0, 0);
+
+		label = new JLabel("NOT SET");
+		label.setForeground(new Color(200, 200, 200));
+
+		panel.add(label);
+
+		panel.setBackground(new Color(0, 0, 0, 200));
+		add(panel, BorderLayout.WEST);
+
+		setOpaque(false);
+		setVisible(true);
+
+		for (Player p : game.getPlayers()) {
+			p.observe(this);
+		}
+	}
+
+	@Override
+	public boolean contains(int x, int y) {
+		for (Component c : this.getComponents()) {
+			if (c.contains(x, y)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void notifyChange(Observable observable) {
+		updateDisplay();
+	}
+
+	private void updateDisplay() {
+		String l = "";
+		int i = 1;
+		for (Player p : game.getPlayers()) {
+			l += "Player " + i++;
+			if (game.getCurrentPlayer() == p) {
+				l += " *";
+			}
+			l += "<br>";
+			l += "Money: " + p.getMoney() + "<br>";
+		}
+		l = "<html>" + l + "</html>";
+		label.setText(l);
+		panel.setSize(label.getPreferredSize());
+		repaint();
+	}
+
+}
